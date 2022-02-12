9a30e60ec8e3a4668dc6ee56624ce2785c6ba0ad
Rafael <rafadevjs@gmail.com>
2022-02-12 01:36:03 -0300

    Refatoracao da pagina

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,13 @@
-//Faq
-button = document.querySelector('.btn');
 
-button.addEventListener('click', ()=>{
-    form = document.querySelector('.forms');
-    form.classList.toggle('hide');
-    contact = document.querySelector('.contact__us');
-    contact.classList.toggle('hide_us');
-});
 
 
 //Menu Mobile 
 function animationMenu(){
-    mobileMenu = document.querySelector(".menu-mobile");
-    mobileMenu.addEventListener('click', ()=>{
+   let mobileMenu = document.querySelector(".menu-toggle");
+   let Menu = document.querySelector('.menu'); 
+   mobileMenu.addEventListener('click', ()=>{
         mobileMenu.classList.toggle('active')
+        Menu.classList.toggle('active')
     });
-    console.log(mobileMenu);
 }
 animationMenu();
